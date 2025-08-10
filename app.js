d21d39f36f8501fe530dd60bc4aef85ed738b635
Rodrigo Velásquez <38990525+Rodrix183@users.noreply.github.com>
2025-08-10 11:29:45 -0600

    Validaciones JS

---
+++ app.js
@@ -0,0 +1,22 @@
+//Funcion del boton de calcular
+document.getElementById("btnCalcular").addEventListener("click", () => {
+const base = parseFloat(document.getElementById("base").value);
+const altura = parseFloat(document.getElementById("altura").value);
+const areaInput = parseFloat(document.getElementById("area"));
+
+
+//Numeros validos
+if(isNaN(base) || isNaN(altura)){
+Swal.fire("Error", "Ingrese valores numericos validos", "error");
+return;
+}
+
+//Numeros positivos
+if(base <= 0 || altura <= 0){
+Swal.fire("Error", "Valores deben ser positivos", "error");
+return;
+
+}
+
+});
+
